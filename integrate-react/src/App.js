9e5c85136219831250132d3d3923a6006b9735cf
Luis Angel Reyes Mendoza <zs24022197@estudiantes.uv.mx>
2026-04-04 11:32:17 -0600

    Agregar un menú de aplicación

--- integrate-react/src/App.js
+++ integrate-react/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
+import { Navbar, Button, Alignment, Classes } from '@blueprintjs/core';
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import logo from './logo.svg';
 import './App.css';
 
   function App  () {
   return (
     <div className="App">
+      <Navbar className={Classes.DARK}>
+  <Navbar.Group align={Alignment.LEFT}>
+    <Navbar.Heading>Mi App electron</Navbar.Heading>
+    <Navbar.Divider />
+    <Button minimal={true} icon="home" text="Inicio" />
+    <Button minimal={true} icon="document" text="Documentos" />
+  </Navbar.Group>
+</Navbar>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <h1>React Electron</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
