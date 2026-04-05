81b59f0454385a530de044d2a87c6ca6c5158943
Luis Angel Reyes Mendoza <zs24022197@estudiantes.uv.mx>
2026-04-04 22:51:49 -0600

    Vuetify 3 instalado porque empresa tacaña no actualizar Vue Material

--- integrate-vue/src/App.vue
+++ integrate-vue/src/App.vue
@@ -1,9 +1,29 @@
-<template>
+<!-- <template>
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+</template> -->
+
+
+<template>
+  <v-app>
+    <v-app-bar color="primary" density="compact">
+      <v-app-bar-title>Mi App Electron + Vuetify</v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-card title="Bienvenidos a mi App" subtitle="Vuetify 3 instalado porque empresa tacaña no actualizar Vue Material" text="Ahora puedes usar todos los componentes de Material Design en tu ventana de Electron.">
+          <v-card-actions>
+            <v-btn color="primary" variant="tonal">Aceptar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <style>
--- integrate-vue/src/main.js
+++ integrate-vue/src/main.js
@@ -1,5 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
-createApp(App).use(router).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    defaultTheme: 'light' 
+  }
+})
+const app = createApp(App)
+
+createApp(App)
+app.use(router)
+app.use(vuetify)
+app.mount('#app')
