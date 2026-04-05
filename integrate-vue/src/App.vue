a79910a149d2d6440a60318f4b022cbfe8c3dbcd
Luis Angel Reyes Mendoza <zs24022197@estudiantes.uv.mx>
2026-04-04 23:07:21 -0600

    Practica vue completada

--- integrate-vue/src/App.vue
+++ integrate-vue/src/App.vue
@@ -9,18 +9,18 @@
 
 <template>
   <v-app>
-    <v-app-bar color="primary" density="compact">
+    <v-app-bar color="primary" density="compact" elevation="2">
       <v-app-bar-title>Mi App Electron + Vuetify</v-app-bar-title>
       <v-spacer></v-spacer>
+            <v-btn variant="text" to="/">Home</v-btn>
+      <v-btn variant="text" to="/about">About</v-btn>
     </v-app-bar>
 
     <v-main>
-      <v-container>
-        <v-card title="Bienvenidos a mi App" subtitle="Vuetify 3 instalado porque empresa tacaña no actualizar Vue Material" text="Ahora puedes usar todos los componentes de Material Design en tu ventana de Electron.">
-          <v-card-actions>
-            <v-btn color="primary" variant="tonal">Aceptar</v-btn>
-          </v-card-actions>
+      <v-container fluid>
+        <v-card title="Bienvenidos a mi App" text="Vuetify 3 instalado porque empresa tacaña no actualizar Vue Material" >
         </v-card>
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
--- integrate-vue/src/views/HomeView.vue
+++ integrate-vue/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Bienvenidos a mi app Vue.js y electron"/>
   </div>
 </template>
 
